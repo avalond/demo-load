3daa9ee2ab45ca700e6f6dae215adf403ad19629
kevin duan <agonyice0115@gmail.com>
2017-05-03 15:38:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kevin/demo/MyService.java
@@ -0,0 +1,22 @@
+package com.example.kevin.demo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+  public MyService() {
+  }
+
+
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    // TODO: Return the communication channel to the service.
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+}
