508622be18bd4d22f6fe794e9b9f016ea20f9991
kevin duan <agonyice0115@gmail.com>
2017-05-04 11:32:23 +0800

    sync code

--- app/src/main/java/com/example/kevin/demo/MainActivity.java
+++ app/src/main/java/com/example/kevin/demo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.kevin.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+  Intent intent;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+     intent= new Intent(MainActivity.this, MyService.class);
+    startService(intent);
+  }
+
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    stopService(intent);
   }
 }
